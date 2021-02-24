db8621ad2340c89514ac18cf755a77ce40708fe3
Tomer Yanko <tmryanko@gmail.com>
2021-02-24 14:53:38 +0200

    phone fix

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -19,7 +19,7 @@ const Info = ({ title }) => {
           htmlColor='#023047'
           style={{ backgroundColor: 'white', borderRadius: 50, padding: 2 }}
         />
-        <InfoCardText href='tel:+49 (0) 30 826 29 13 - 0'>
+        <InfoCardText href='tel:+49 030 826 29 13 - 0'>
           +49 (0) 30 826 29 13 - 0
         </InfoCardText>
       </InfoCard>
