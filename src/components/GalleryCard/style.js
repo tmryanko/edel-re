8348126cb9fbfce5c07b1273012684b676d88a8e
Tomer Yanko <tmryanko@gmail.com>
2021-03-01 17:48:15 +0200

    gallery img

--- src/components/GalleryCard/style.js
+++ src/components/GalleryCard/style.js
@@ -4,17 +4,17 @@ export const GalleryCardCont = styled.div`
   box-sizing: border-box;
   height: 456px;
   width: 646px;
-  margin: 20px;
+  margin: 10px;
   transition: transform 0.5s ease;
   @media (min-width: 768px) and (max-width: 1415px) {
     height: 406px;
     width: 540px;
-    margin: 10px;
+    margin: 5px;
   }
   @media (max-width: 768px) {
     height: 146px;
     width: 170px;
-    margin: 10px;
+    margin: 2px;
   }
   @media (min-width: 768px) {
     &:hover {
