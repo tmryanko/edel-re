c4a13b18b063156f2d453e037b55549be0a837b0
Tomer Yanko <tmryanko@gmail.com>
2021-03-04 13:35:56 +0200

    comment card

--- src/components/Comments/style.js
+++ src/components/Comments/style.js
@@ -9,7 +9,13 @@ export const CommentsCont = styled.div`
   @media (max-width: 768px) {
     width: 95%;
   }
-  @media (min-width: 768px) and (max-width: 1250px) {
+  @media (min-width: 768px) and (max-width: 1000px) {
+    width: 85%;
+  }
+  @media (min-width: 1000px) and (max-width: 1250px) {
     width: 65%;
+  }
+  @media (min-width: 1250px) and (max-width: 1550px) {
+    width: 55%;
   }
 `;
