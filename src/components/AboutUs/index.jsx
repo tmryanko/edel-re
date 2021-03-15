09035ce2e13621d8f32f4dcfe451924c4ff7c36d
Tomer Yanko <tmryanko@gmail.com>
2021-03-15 09:59:08 +0200

    s

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -9,16 +9,16 @@ const AboutUs = ({ title, lang }) => {
   const TEXT = {
     English: {
       about: `
-      EDEL Real Estate Gmbh is an established company in the real estate business. This is our passion, so we work around the clock for buyers and sellers to accommodate their best interests. Over the years we have accumulated great experience in the sector to meet the needs of various kinds of investors.
+      EDEL Real Estate GmbH is an established company in the real estate business. This is our passion, so we work around the clock for buyers and sellers to accommodate their best interests. Over the years we have accumulated great experience in the sector to meet the needs of various kinds of investors.
 
 Our areas of expertise are acquisition, management, brokerage and sale of real estate all over Germany.
       `,
     },
     Deutschland: {
       about: `
-      Die EDEL Real Estate Gmbh ist ein etabliertes Unternehmen in der Immobilienbranche. Dieses Geschäft ist unsere Leidenschaft und deshalb arbeiten wir rund um die Uhr für Käufer und Verkäufer, um ihren Interessen bestmöglich gerecht zu werden. Über die Jahre hinaus haben wir große Erfahrungen in der Immobilienbranche gesammelt, um gezielt auf den Bedürfnissen der verschiedenen Investoren einzugehen.
+      Die EDEL Real Estate GmbH ist ein etabliertes Unternehmen in der Immobilienbranche. Dieses Geschäft ist unsere Leidenschaft und deshalb arbeiten wir rund um die Uhr für Käufer und Verkäufer, um ihren Interessen bestmöglich gerecht zu werden. Über die Jahre hinaus haben wir große Erfahrungen in der Immobilienbranche gesammelt, um gezielt auf den Bedürfnissen der verschiedenen Investoren einzugehen.
 
-Unsere Schwerpunkte liegen in den Bereichen des Ankaufs, Verkauf, der Vermietung und der Vermögensverwaltung.
+Unsere Schwerpunkte liegen in den Bereichen des Ankaufs, Verkaufs, der Vermietung und der Vermögensverwaltung.
 
 
       `,
